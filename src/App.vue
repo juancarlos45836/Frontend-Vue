f4faf8cdb610772e4b1f5d936d6909f40c6b7837
CharlyCodigo <juancarlos45836@gmail.com>
2024-03-22 19:32:36 -0600

    add tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,11 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div class="bg-[#FFE600] absolute w-ful h-auto top-0 left-0">
+      <label class="">Hola Mundo</label>
+      <input type="text" class="w-8 h-auto" />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <main></main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>

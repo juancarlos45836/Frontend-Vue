ebc551f2ca18fe8da72e528fcfa631b059d6a03e
CharlyCodigo <juancarlos45836@gmail.com>
2024-04-09 17:04:56 -0600

    Menu desplegable en boton responsivo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+var btnMenuClicked = ref(false)
+console.log(btnMenuClicked)
+const clicked = () => {
+  btnMenuClicked.value === false ? (btnMenuClicked.value = true) : (btnMenuClicked.value = false)
+  console.log(btnMenuClicked)
+}
+</script>
 <template>
   <div class="w-full h-auto">
     <header
@@ -9,12 +17,14 @@
           <div
             class="flex flex-col py-0 ml-2 md:ml-0 justify-center md:justify-between md:w-[140px] cursor-pointer md:py-3"
           >
+            <!--Insercion de iconos en modo responsivo-->
             <img src="../assets/logomercado.png" class="visible md:hidden w-[90%]" alt="" />
             <img
               class="hidden md:visible md:flex w-0 md:w-[140px]"
               src="../images/logo_large_plus.webp"
               alt=""
             />
+            <!--cierre de bloque-->
             <div class="hidden md:visible w-[140px] md:flex flex-row">
               <img class="h-auto w-[20px]" src="../assets/ubicacion.png" alt="ubicacion" />
               <p class="text-[13px] w-full cursor-pointer">Ingresa tu domicilio</p>
@@ -47,8 +57,9 @@
               <p class="mr-2 cursor-pointer">Ayuda</p>
             </div>
           </div>
+          <!--Icono de menu para el modo responsivo-->
           <div class="w-[100px] md:hidden ml-1 pl-1 flex items-center flex-row">
-            <img class="w-[20px]" src="../assets/menu.png" alt="" />
+            <img class="w-[20px]" v-on:click="clicked" src="../assets/menu.png" alt="" />
             <img
               class="w-[20px] h-[20px] ml-1 cursor-pointer"
               src="../assets/carrito.png  "
@@ -76,6 +87,83 @@
         </div>
       </div>
     </header>
+  </div>
+  <div
+    class="md:hidden flex w-full border-[1px] border-gray-300 border-solid"
+    v-if="btnMenuClicked"
+  >
+    <div class="w-full">
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 mt-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/casita.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Inicio</p>
+      </div>
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/notificacion.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Notificaciones</p>
+      </div>
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/tienda.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Mis Compras</p>
+      </div>
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/favorito.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Favoritos</p>
+      </div>
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/etiqueta.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Ofertas</p>
+      </div>
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/adquisitivo.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Creditos</p>
+      </div>
+      <div class="flex flex-row pl-10 h-[50px] w-[100vh] py-5 mb-5 hover:bg-[#F7F7F7]">
+        <div class="flex justify-center items-center">
+          <img
+            class="w-[20px] h-[20px] flex justify-center items-center"
+            src="../assets/suscripcion.png"
+            alt="home"
+          />
+        </div>
+        <p class="text-sm flex justify-center items-center ml-5">Suscripciones</p>
+      </div>
+    </div>
   </div>
 </template>
 >

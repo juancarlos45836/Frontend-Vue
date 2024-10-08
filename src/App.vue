23bacd8a0831ca73c9e68b325716d8050bb7dcca
CharlyCodigo <juancarlos45836@gmail.com>
2024-10-07 21:41:50 -0600

    Actalizacion de la navegacion login, register, dashboard

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import Header from './components/Header.vue'
-import Main from './components/Main.vue'
+<script setup>
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <Header />
-  <Main />
+  <div class="flex flex-col">
+    <Navbar class="w-full h-auto" v-if="$route.fullPath === '/dashboard'">este es un bar</Navbar>
+    <div class="w-full h-auto">
+      <RouterView />
+    </div>
+  </div>
 </template>
